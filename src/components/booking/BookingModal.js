51329f572a55a1dca2d04ff59c7bb8bbc785241c
Bijay Maharjan <bijay.maharjan5@gmail.com>
2020-01-31 21:26:52 -0600

    Add city search endpoints backend

--- src/components/booking/BookingModal.js
+++ src/components/booking/BookingModal.js
@@ -22,7 +22,7 @@ export function BookingModal(props) {
             </div>
             <ResError errors={errors} />
             <div className='modal-footer'>
-                <button onClick={confirmModal} type='button' className='btn btn-bwm'>Confirm</button>
+                <button onClick= {confirmModal} type='button' className='btn btn-bwm'>Confirm</button>
                 <button type='button' onClick={closeModal} className='btn btn-bwm'>Cancel</button>
             </div>
         </Modal>
