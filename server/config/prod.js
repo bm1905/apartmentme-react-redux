6c89463e9e6889c1c52ece543eb5614dadc108b7
Bijay Maharjan <bijay.maharjan5@gmail.com>
2020-04-20 22:19:09 -0500

    Fix Heroku H10 bug

--- server/config/prod.js
+++ server/config/prod.js
@@ -1,9 +1,9 @@
 module.exports = {
     DB_URI: process.env.DB_URI,
-    SECRET: process.env.SECRET
-    // AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
-    // AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
-    // GOOGLE_MAP_URI: process.env.GOOGLE_MAP_URI,
-    // EMAIL: process.env.EMAIL,
-    // PASSWORD: process.env.PASSWORD
+    SECRET: process.env.SECRET,
+    AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
+    AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
+    GOOGLE_MAP_URI: process.env.GOOGLE_MAP_URI,
+    EMAIL: process.env.EMAIL,
+    PASSWORD: process.env.PASSWORD
 }
