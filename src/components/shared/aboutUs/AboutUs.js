229d624e6e6d8f8bddc53782ba1f323151d737ce
Bijay Maharjan <bijay.maharjan5@gmail.com>
2020-04-24 00:26:45 -0500

    Added About Us

--- src/components/shared/aboutUs/AboutUs.js
+++ src/components/shared/aboutUs/AboutUs.js
@@ -2,53 +2,58 @@ import React from 'react';
 
 export const AboutUs = () => {
     return (
-        <div class="aboutus-section">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-3 col-sm-6 col-xs-12">
-                        <div class="aboutus">
-                            <h2 class="aboutus-title">About Us</h2>
-                            <p class="aboutus-text">Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in.</p>
-                            <p class="aboutus-text">This is Photoshop's version of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum, nec sagittis sem</p>
+        <React.Fragment>
+            <div className="aboutus-section">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-7 col-sm-6 col-xs-12">
+                            <div className="aboutus">
+                                <h2 className="aboutus-title">ApartmentMe</h2>
+                                <p className="aboutus-text">ApartmentMe is a web application that serves a variety of customers seeking homes.
+                                We allow users the option to list their apartment, house, or just a single room for rent.
+                                Similarly, the users can browse for an available house, apartment, or room using our application.
+                                The website provides all the required features to ensure a smooth transition for moving families, students, and travelers.
+                            </p>
+                                <p className="aboutus-text">Our mission is empowering home seekers by making their journey as simple and efficient as possible.</p>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-md-3 col-sm-6 col-xs-12">
-                        <div class="aboutus-banner">
-                            {/* <img src="http://themeinnovation.com/demo2/html/build-up/img/home1/about1.jpg" alt="" /> */}
-                    </div>
+                        {/* <div className="col-md-3 col-sm-6 col-xs-12">
+                        <div className="aboutus-banner">
+                            <img src="http://themeinnovation.com/demo2/html/build-up/img/home1/about1.jpg" alt="" />
                         </div>
-                        <div class="col-md-5 col-sm-6 col-xs-12">
-                            <div class="feature">
-                                <div class="feature-box">
-                                    <div class="clearfix">
-                                        <div class="iconset">
-                                            <span class="glyphicon glyphicon-cog icon"></span>
+                    </div> */}
+                        <div className="col-md-5 col-sm-6 col-xs-12">
+                            <div className="feature">
+                                <div className="feature-box">
+                                    <div className="clearfix">
+                                        <div className="iconset">
+                                            <span className="glyphicon glyphicon-cog icon"><i className="fa fa-graduation-cap"></i></span>
                                         </div>
-                                        <div class="feature-content">
-                                            <h4>Work with heart</h4>
-                                            <p>Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in.</p>
+                                        <div className="feature-content">
+                                            <h4>For Students</h4>
+                                            <p>Students can browse all off-campus apartments around their selected colleges/universities.</p>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="feature-box">
-                                    <div class="clearfix">
-                                        <div class="iconset">
-                                            <span class="glyphicon glyphicon-cog icon"></span>
+                                <div className="feature-box">
+                                    <div className="clearfix">
+                                        <div className="iconset">
+                                            <span className="glyphicon glyphicon-cog icon"><i className="fa fa-home"></i></span>
                                         </div>
-                                        <div class="feature-content">
-                                            <h4>Reliable services</h4>
-                                            <p>Donec vitae sapien ut libero venenatis faucibu. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt</p>
+                                        <div className="feature-content">
+                                            <h4>For People wanting temporary stay</h4>
+                                            <p>Users can browse for single room rentals to rent and stay for very few period of time.</p>
                                         </div>
                                     </div>
                                 </div>
-                                <div class="feature-box">
-                                    <div class="clearfix">
-                                        <div class="iconset">
-                                            <span class="glyphicon glyphicon-cog icon"></span>
+                                <div className="feature-box">
+                                    <div className="clearfix">
+                                        <div className="iconset">
+                                            <span className="glyphicon glyphicon-cog icon"><i className="fa fa-users"></i></span>
                                         </div>
-                                        <div class="feature-content">
-                                            <h4>Great support</h4>
-                                            <p>Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in.</p>
+                                        <div className="feature-content">
+                                            <h4>For people looking for roomates</h4>
+                                            <p>Users can post their rooms for rental if any rooms in their house/apartment are vacant.</p>
                                         </div>
                                     </div>
                                 </div>
@@ -57,5 +62,54 @@ export const AboutUs = () => {
                     </div>
                 </div>
             </div>
+
+            <section className="our-webcoderskull padding-lg">
+                <div className="container">
+                    <div className="row heading heading-icon">
+                        <h2>Our Team</h2>
+                    </div>
+                    <ul className="row">
+                        <li className="col-12 col-md-6 col-lg-4">
+                            <div className="cnt-block equal-hight" style={{ height: "349px" }}>
+                                <figure> <img src={process.env.PUBLIC_URL + '/img/bm.png'} className="img-responsive" alt="" /></figure>
+                                <h3><a href="#">Bijay Maharjan</a></h3>
+                                <p>Full-Stack Web Developer</p>
+                                <ul className="follow-us clearfix">
+                                    <li><a href="#"><i className="fa fa-facebook" aria-hidden="true"></i></a></li>
+                                    <li><a href="#"><i className="fa fa-envelope" aria-hidden="true"></i></a></li>
+                                    <li><a href="#"><i className="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li className="col-12 col-md-6 col-lg-4">
+                            <div className="cnt-block equal-hight" style={{ height: "349px" }}>
+                                <figure><img src={process.env.PUBLIC_URL + '/img/luji.png'} className="img-responsive" alt="" /></figure>
+                                <h3><a href="#">Lujien Sallam </a></h3>
+                                <p>Full-Stack Web Developer</p>
+                                <ul className="follow-us clearfix">
+                                    <li><a href="#"><i className="fa fa-facebook" aria-hidden="true"></i></a></li>
+                                    <li><a href="#"><i className="fa fa-envelope" aria-hidden="true"></i></a></li>
+                                    <li><a href="#"><i className="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li className="col-12 col-md-6 col-lg-4">
+                            <div className="cnt-block equal-hight" style={{ height: "349px" }}>
+                                <figure><img src={process.env.PUBLIC_URL + '/img/kenisha.png'} className="img-responsive" alt="" /></figure>
+                                <h3><a href="#">Kenisha Lewis </a></h3>
+                                <p>Full-Stack Web Developer</p>
+                                <ul className="follow-us clearfix">
+                                    <li><a href="#"><i className="fa fa-facebook" aria-hidden="true"></i></a></li>
+                                    <li><a href="#"><i className="fa fa-envelope" aria-hidden="true"></i></a></li>
+                                    <li><a href="#"><i className="fa fa-linkedin" aria-hidden="true"></i></a></li>
+                                </ul>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </section>
+        </React.Fragment>
     )
+
+
 }
